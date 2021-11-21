4dd046d14c1e60823bf7588632ef0eab1b0d0fd1
ttdesign <tkthedesigner@gmail.com>
2021-11-20 21:56:10 -0800

    add redux store, services and fetch data from api

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.css'
 import {BrowserRouter as Router} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './app/store'
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Router>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</Router>
 	</React.StrictMode>,
 	document.getElementById('root')
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+	'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+	'x-rapidapi-key': '651f129c5dmsh77b3f9006c2b46fp169f6ajsn82622810010b',
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest =(url)=> ({
+    url, headers:cryptoApiHeaders
+})
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints:(builder) => ({
+        getCryptos: builder.query({
+            query:() => createRequest('/coins')
+        })
+    })
+});
+export const {
+    useGetCryptosQuery,
+} = cryptoApi
