ec2b4567a27a64d7e6671b1617dfa6c6d0aa64d3
ttdesign <tkthedesigner@gmail.com>
2021-12-03 23:11:20 -0800

    add cryptodetail page, render detail, styling detail page

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -11,14 +11,17 @@ const createRequest =(url)=> ({
     url, headers:cryptoApiHeaders
 })
 export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({baseUrl}),
-    endpoints:(builder) => ({
-        getCryptos: builder.query({
-            query:(count) => createRequest(`/coins?limit=${count}`)
-        })
-    })
+	reducerPath: 'cryptoApi',
+	baseQuery: fetchBaseQuery({ baseUrl }),
+	endpoints: (builder) => ({
+		getCryptos: builder.query({
+			query: (count) => createRequest(`/coins?limit=${count}`),
+		}),
+		getCryptoDetails: builder.query({
+			query: (coinId) => createRequest(`/coin/${coinId}`),
+		}),
+	}),
 });
 export const {
-    useGetCryptosQuery,
+    useGetCryptosQuery, useGetCryptoDetailsQuery
 } = cryptoApi
