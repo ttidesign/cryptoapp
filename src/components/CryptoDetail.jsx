46c446940164727ec123c382c6ee847134b031ff
ttdesign <tkthedesigner@gmail.com>
2021-12-04 15:18:12 -0800

    add graph and line chart

--- src/components/CryptoDetail.jsx
+++ src/components/CryptoDetail.jsx
@@ -4,14 +4,16 @@ import {useParams} from 'react-router-dom'
 import millify from 'millify'
 import {Col, Row, Typography, Select} from 'antd'
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
-import {useGetCryptoDetailsQuery} from '../services/cryptoApi'
+import {useGetCryptoDetailsQuery, useGetCryptoHistoryQuery} from '../services/cryptoApi'
+import LineChart from './LineChart';
 
 const {Title, Text} = Typography
 const {Option} = Select
 function CryptoDetail () {
     const {coinId} = useParams()
-    const [tinePeriod, setTimePeriod] = useState('7d')
+    const [timePeriod, setTimePeriod] = useState('7d')
     const {data, isFetching} = useGetCryptoDetailsQuery(coinId)
+    const {data: coinHistory} = useGetCryptoHistoryQuery({coinId, timePeriod})
     const cryptoDetails = data?.data?.coin
     if (isFetching) return 'Loading...'
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
@@ -42,6 +44,7 @@ function CryptoDetail () {
             <Select defaultValue='7d' className='select-timeperiod' placeholder='Select Time Period' onChange={(value) =>setTimePeriod(value)}> 
             {time.map((date)=> <Option key={date}> {date} </Option>)}
             </Select>
+            <LineChart coinHistory ={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name} /> 
             <Col className='stats-container'>
                 <Col className='coin-value-statistics'> 
                     <Col className='coin-value-statistics-heading'> 
